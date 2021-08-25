fff82da16b3ee8d005460858314c478804588a2f
Gato <Roy.Hagmansen@gmail.com>
2021-08-25 13:54:10 +0200

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblig1_Modul3
+{
+    class Person
+    {
+		public int myID = 100;
+		public int ID { get; set; } = 0;
+		public string FirstName { get; set; } = "";
+		public string LastName { get; set; } = "";
+		public string BirthYear { get; set; } = "";
+		public string DeathYear { get; set; } = "";
+		public string Father { get; set; } = "";
+		public string Mother { get; set; } = "";
+
+		public void printInfo()
+		{
+			Console.WriteLine("\tID        : " + ID);
+			if (FirstName != "")Console.WriteLine("\tFirst name: " + FirstName);
+			if (LastName != "") Console.WriteLine("\tLast name : " + LastName);
+			if (BirthYear != "") Console.WriteLine("\tBirth     : " + BirthYear);
+			if (DeathYear != "") Console.WriteLine("\tDeath     : " + DeathYear);
+			if (Father != "") Console.WriteLine("\tFather    : " + Father);
+			if (Mother != "") Console.WriteLine("\tMother    : " + Mother);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oblig1_Modul3
+{
+    class Program
+    {
+		static void Main()
+		{
+			int i;
+			int j=0;
+			List<Person> F = new List<Person>(); 
+
+			F.Add(new Person { ID = 101, FirstName= "Bert", LastName = "Verin", BirthYear = "1900", DeathYear = "1960" });
+			F.Add(new Person { ID = 102, FirstName = "Misha", LastName = "Verin", BirthYear = "1900", DeathYear = "1960" });
+			F.Add(new Person { ID = 103, FirstName = "Potter", LastName = "Verin", BirthYear = "1920", DeathYear = "1950" });
+			F.Add(new Person { ID = 104, FirstName = "Vladi", LastName = "Verin", BirthYear = "1920", DeathYear = "1980" });
+			F.Add(new Person { ID = 105, FirstName = "Petrosh", LastName = "Verin", BirthYear = "1920", DeathYear = "1994" });
+			F.Add(new Person { ID = 106});
+			F.Add(new Person { ID = 107, LastName = "Test" });
+			F[2].Mother = "Misha";
+			F[2].Father = "Bert";
+			F[3].Mother = "Misha";
+			F[3].Father = "Bert";
+
+			while (j < F.Count)
+            {
+				if (F[j].ID == 0)
+				{
+					return;
+				}
+				else
+				{
+					F[j].printInfo();
+					j++;
+				}
+			}
+		}
+	}
+}
